26921bf082fa705f62c301d60f52b21bb94b6446
kelvbutler <kelvbutler@gmail.com>
2017-01-13 15:28:49 +1100

    bits and pieces
    adds efficiant cleaning and updateing as well as some comments

--- StandardGame/Enitity.lua
+++ StandardGame/Enitity.lua
@@ -11,20 +11,37 @@ Entity.CompressEntities=function(self)
 	end
 	self.entities=cond
 end
+--updates and cleans entities
+Enitity.Update=function(self)
+	cond={}
+	for k,v in pairs(self.entities) do
+		if v.isActive then
+			v.key=#cond+1
+			cond[#cond+1]=v
+			v:update()
+		end
+	end
+	self.entities=cond
+end
 
-Entity.new= function(health,speed,position,size)
+Entity.new= function(name,health,speed,position,size)
 	newEntity={}
+	-- name is used to determain tpye eg "player","zombie","bullet"
+	newEntity.name=name
 	newEntity.health=health
 	newEntity.speed=speed
 	newEntity.position=position
 	newEntity.size=size
 	new.isActive=true
+	--override this with entity logic
 	newEntity.Update=function(self,dt)
 	
 	end
+	--override this with enity draw
 	newEntity.Draw=function(self)
 		love.graphics.rectangle("fill",self.position.x,self.position.y,size,size)
 	end
+	
 	--should return table of entities colliding with self - untested- may return copy rather than original
 	newEntity.CheckCollision=function(self)
 		collisions={}
@@ -39,5 +56,13 @@ Entity.new= function(health,speed,position,size)
 		end
 		return collisions
 	end
+	--imidiatly deactivates and compresses entity - may be a little expensive
+	newEntity.Destroy=function(self)
+		self.isActive=false
+		Entity:CompressEntities()
+	end
+	--key is it's index in entities - may become nessasary for referance
+	newEntity.key=#Entity.entities+1
+	Entity.entities[newEntity.key]=newEntity
 	return newEntity
 end
