87fa1f38e8ce718b5ce62facc779647f019fee65
FLOFFY_ <lachixd@gmail.com>
2017-01-13 15:08:46 +1100

    Config

--- StandardGame/main.lua
+++ StandardGame/main.lua
@@ -1,19 +1,25 @@
 function love.load()
+  
+  -- PLAYER
 	player={}
 	player.speed=100
 	player.health=10
 	player.ammo=10
 	player.position={x=0,y=0}
+  player.size = 50
 	player.Move = function(self,direction,dt)
 		self.position.x=self.position.x+(self.speed*dt*direction.x)
 		self.position.y=self.position.y+(self.speed*dt*direction.y)
+  end
 	player.Fire = function(direction)
 		ammo=ammo-1
 	end
-	end
+  
 end
 
 function love.update(dt)
+  
+  -- PLAYER MOVEMENT
 	if love.keyboard.isDown("w") then
 		player:Move({x=0,y=-1},dt)
 	end
@@ -26,8 +32,9 @@ function love.update(dt)
 	if love.keyboard.isDown("d") then
 		player:Move({x=1,y=0},dt)
 	end
+  
 end
 
 function love.draw()
-	love.graphics.rectangle("fill",player.position.x,player.position.y,50,50)
+	love.graphics.rectangle("fill",player.position.x,player.position.y,player.size,player.size)
 end
