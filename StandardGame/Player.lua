d6df706dbf919c7fe782e4847dab2f774084e970
FLOFFY_ <lachixd@gmail.com>
2017-01-14 15:01:16 +1100

    WindowCheck

--- StandardGame/Player.lua
+++ StandardGame/Player.lua
@@ -3,18 +3,30 @@ Player={}
 Player.new=function(health,speed,position,size,ammo)
 	newPlayer=Entity.new("player",health,speed,position,size)
 	newPlayer.ammo=ammo
+  
+  newPlayer.Move = function(self,direction,dt)
+		self.ppx=self.position.x+(self.speed*dt*direction.x)
+		self.ppy=self.position.y+(self.speed*dt*direction.y)
+    if (0 < self.ppx) and (self.ppx < love.graphics.getWidth()-self.size) then
+      self.position.x = self.ppx
+    end
+    if (0 < self.ppy) and (self.ppy < love.graphics.getHeight()-self.size) then
+      self.position.y = self.ppy
+    end
+  end
+    
 	newPlayer.Update=function(self,dt)
 		if love.keyboard.isDown("w") then
-		self.position.y=self.position.y-(self.speed*dt)
+      newPlayer:Move({x=0,y=-1},dt)
 		end
 		if love.keyboard.isDown("s") then
-			self.position.y=self.position.y+(self.speed*dt)
+			newPlayer:Move({x=0,y=1},dt)
 		end
 		if love.keyboard.isDown("a") then
-			self.position.x=self.position.x-(self.speed*dt)
+			newPlayer:Move({x=-1,y=0},dt)
 		end
 		if love.keyboard.isDown("d") then
-			self.position.x=self.position.x+(self.speed*dt)
+			newPlayer:Move({x=1,y=0},dt)
 		end
 	end
 	return newplayer
--- StandardGame/main.lua
+++ StandardGame/main.lua
@@ -3,39 +3,9 @@ require "Player"
 
 function love.load()
   player=Player.new(10,20,{x=0,y=0},25,10)
-  -- PLAYER
-  
-	--[[player={}
-	player.speed=100
-	player.health=10
-	player.ammo=10
-	player.position={x=0,y=0}
-  player.size = 50
-	player.Move = function(self,direction,dt)
-		self.position.x=self.position.x+(self.speed*dt*direction.x)
-		self.position.y=self.position.y+(self.speed*dt*direction.y)
-  end
-	player.Fire = function(direction)
-		ammo=ammo-1
-	end]]--
-  
 end
 
 function love.update(dt)
-  
-  -- PLAYER MOVEMENT
-	--[[if love.keyboard.isDown("w") then
-		player:Move({x=0,y=-1},dt)
-	end
-	if love.keyboard.isDown("s") then
-		player:Move({x=0,y=1},dt)
-	end
-	if love.keyboard.isDown("a") then
-		player:Move({x=-1,y=0},dt)
-	end
-	if love.keyboard.isDown("d") then
-		player:Move({x=1,y=0},dt)
-	end]]--
 	Entity:Update(dt)
   
 end
